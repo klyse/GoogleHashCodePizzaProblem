91325a7cbdfc7a1c3b5d9a4a95b39c79251cf773
klyse <Klaus.Pruenster@gmail.com>
2019-02-27 11:49:15 +0100

    Update EnvironmentConstants.cs

--- GoogleHashCodePizzaProblem/EnvironmentConstants.cs
+++ GoogleHashCodePizzaProblem/EnvironmentConstants.cs
@@ -5,7 +5,7 @@ namespace GoogleHashCodePizzaProblem
 {
 	public static class EnvironmentConstants
 	{
-		public static string EnvironmentVariable { get; } = "HashCodeWorkingDir";
+		public static string EnvironmentVariable { get; } = "HashCodePizzaWorkingDir";
 		public static string DataPath => Environment.GetEnvironmentVariable(EnvironmentVariable);
 		public static string InputPath => Path.Combine(DataPath, "Input");
 		public static string OutputPath => Path.Combine(DataPath, "Output");
